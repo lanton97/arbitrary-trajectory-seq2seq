d3c9066f955aff2946fc187fe226f90e5098e8cf
lanton97 <15la@queensu.ca>
2025-10-16 18:28:39 -0400

    Remove deprecated bullet_sim stuff

--- simulations/bullet_sim/environments/__init__.py
+++
@@ -1,6 +0,0 @@
-from gymnasium.envs.registration import register
-from ..environments.wheeled_mobile_robot.turtlebot.turtlebot import Turtlebot
-register(
-    id='turtlebot-v0',
-    entry_point='environments.wheeled_mobile_robot.turtlebot.turtlebot:Turtlebot',
-)
