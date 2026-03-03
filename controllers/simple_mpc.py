aef1727a2bb7d883cf59d702ea36327ccd2e2c1d
lanton97 <15la@queensu.ca>
2026-03-02 21:04:35 -0500

    ome MPC improvements

--- controllers/simple_mpc.py
+++ controllers/simple_mpc.py
@@ -1,5 +1,4 @@
 import numpy as np
-import cvxpy as cp
 import time
 from roboticstoolbox.mobile.drivers import VehicleDriverBase
 from roboticstoolbox.mobile import Unicycle
@@ -38,61 +37,53 @@ def two_dim_from_flat(flat_list, dim):
 
 
 # Cost function to be minimized
-def mpc_cost(u, u_ini, x0, traj,  Q, R, N):
-
-    # Initialise cost = 0 and states to current measured states
+def mpc_cost(u, u_ini, x0, traj, Q, R, N):
     cost = 0
-    x = x0 
-
-    # Simulate the next N steps of the system
-    for idx in range(0, N, 2):
-        inp = [u[idx], u[idx+1]]
+    x = np.copy(x0)
+    # Penalize input changes for smoothness
+    prev_u = np.copy(u_ini[:2])
+    for idx in range(0, N*2, 2):
+        inp = np.array([u[idx], u[idx+1]])
         x = unicycle_step(x, inp)
-        inp = np.array(inp)
-
-        cost +=  (x[:2] - traj[idx][:2]).T@Q @ (x[:2] - traj[idx][:2])
-        cost += (inp).T@R @ inp
+        # Tracking cost (position)
+        step_idx = idx // 2
+        cost += (x[:2] - traj[step_idx][:2]).T @ Q @ (x[:2] - traj[step_idx][:2])
+        # Heading cost (optional, penalize angle error)
+        th_err = np.arctan2(np.sin(x[2] - traj[step_idx][2]), np.cos(x[2] - traj[step_idx][2]))
+        cost += 0.5 * th_err**2
+        # Input cost
+        cost += inp.T @ R @ inp
+        # Input change penalty
+        cost += 0.5 * np.sum((inp - prev_u)**2)
+        prev_u = inp
+    # Terminal cost (strong penalty for final position and heading)
+    cost += 5.0 * (x[:2] - traj[-1][:2]).T @ Q @ (x[:2] - traj[-1][:2])
+    th_err = np.arctan2(np.sin(x[2] - traj[-1][2]), np.cos(x[2] - traj[-1][2]))
+    cost += 1.0 * th_err**2
     return cost
 
-# Adapted from https://github.com/tsender/me561-unicycle-mpc/blob/master/turtlebot_mpc/src/turtlebot_mpc/unicycle_mpc.py
 class SimpleMPC(ControllerWrapper):
     def __init__(
         self,
-        speed=2,
+        speed=1,
         seed=0,
         dt=0.1,
-        horizon=25,
+        horizon=12,
         vehicle=None,
-        relativeState=False
+        relativeState=True
     ):
         self.n = 3 # 3 states (x, y, theta)
         self.m = 2 
-
         self._speed = speed
         self._goal_marker = None
         self._t = 0
         self._dt=dt
         self._last_v = np.array([0.0,0.0])
         self.horizon = horizon
-
-        N = 50
-        traj = np.zeros((N, 3))
-
-        #self.Q = np.array([[0.1,0.0,0.0],
-        #             [0.0,0.1,0.0],
-        #             [0.0,0.0,0.0]])
-
-        self.Q = np.array([[0.1,0.0],
-                     [0.0,0.1]])
-
-        self.R = np.array([[0.001, 0.0],
-                      [0.0, 0.001]])
-
+        self.Q = np.array([[0.6,0.0],[0.0,0.6]])
+        self.R = np.array([[0.1, 0.0],[0.0, 0.1]])
         self.u_ini = np.zeros(horizon*2)
-
-        self.uMax = np.array([1.0,1.0])
-
-
+        self.uMax = np.array([3.0,3.0])
         self._seed = seed
         self.verbose = True
         self._veh = vehicle
@@ -101,49 +92,24 @@ class SimpleMPC(ControllerWrapper):
         self._W = 1.0
         self.relativeState = relativeState
 
-    def init(self, initObs):
-        pass
-
-
-    # Solve MPC optimization problem
     def solve_mpc(self, x0, traj):
-        # Bounds --> -tau_max <= tau[idx] <= tau_max for idx = 0 to N-1
+        # Pad trajectory if needed
+        traj_pad = np.zeros((self.horizon, 3))
+        for i in range(min(self.horizon, len(traj))):
+            traj_pad[i,:] = traj[i]
         bounds = [(-self.uMax[idx%2], self.uMax[idx%2]) for idx in range(self.horizon*2)]
- 
-        # Starting optimisation point for theta and dtheta are the current measurements
- 
-        # Minimization
-        result = minimize(mpc_cost, self.u_ini, args=(self.u_ini,x0, traj, self.Q, self.R, self.horizon), bounds=bounds,method='SLSQP')
- 
-        # Extract the optimal control sequence
+        result = minimize(mpc_cost, self.u_ini, args=(self.u_ini, x0, traj_pad, self.Q, self.R, self.horizon), bounds=bounds, method='SLSQP')
         u_mpc = two_dim_from_flat(result.x, 2)
- 
         return u_mpc
 
     def demand(self, s, traj, noise):
-        """
-        Compute speed and heading for timestep
-            %
-            % [SPEED,STEER] = R.demand() is the speed and steer angle to
-            % drive the vehicle toward the next waypoint.  When the vehicle is
-            % within R.dtresh a new waypoint is chosen.
-            %
-            % See also Vehicle."""
-
         speed = self._speed
-        #traj = [t.numpy() for t in traj]
         if self.relativeState:
             s = np.array([0.0,0.0,0.0])
-
         us = self.solve_mpc(s, traj)
-        # Save for warmstart on next minimization
         self.u_ini = to_flat(us)
-
         v = us[0]
-
-        v += np.random.multivariate_normal(np.zeros(2), noise)
         self.inputs.append(v)
-
         return self.make_control_callable(v)
 
     def make_control_callable(self, u):
